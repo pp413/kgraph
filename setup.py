d6ab7a1a9f3d4be0de0c20faed2c44894eebcff1
pp413 <shuanglongyao@gmail.com>
2021-03-02 17:22:48 +0800

    version 0.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup_params = dict(name='kgraph',
-                    version='0.4.0',
+                    version='0.4.1',
                     description='A Python library for relational learning on knowledge graphs.',
                     url='https://github.com/YaoShuang-long/kgraph',
                     author='Yao Shuang-long',
